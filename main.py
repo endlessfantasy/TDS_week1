b9abc8ede90cee24b27431b869862116a8493209
endlessfantasy <endless.fantasykawai@gmail.com>
2025-01-28 12:18:05 +0530

    Added fastapi app for vercel

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+from typing import List
+import json
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
+
+with open("data.json") as f:
+    data = json.load(f)
+
+@app.get("/api")
+async def get_marks(name: List[str] = Query(...)):
+    marks = [entry["marks"] for entry in data if entry["name"] in name]
+    return {"marks": marks}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
